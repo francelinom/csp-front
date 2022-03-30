e1be4b5f92e33baf43362246121ce5c887fc9782
francelino <fmarcilio1989@gmail.com>
2022-03-29 22:33:44 -0300

    formulario de cadastro de cliente

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,15 +1,15 @@
+import { Cliente } from './../cliente';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-clientes-form',
   templateUrl: './clientes-form.component.html',
-  styleUrls: ['./clientes-form.component.css']
+  styleUrls: ['./clientes-form.component.css'],
 })
 export class ClientesFormComponent implements OnInit {
+  cliente?: Cliente;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
