12e3fe44e73c4521b3c415031fcdb5fc1b22ef51
francelino <fmarcilio1989@gmail.com>
2022-04-03 21:57:10 -0300

    construindo modal de deletar cliente

--- src/app/clientes/clientes-lista/clientes-lista.component.ts
+++ src/app/clientes/clientes-lista/clientes-lista.component.ts
@@ -10,6 +10,7 @@ import { Router } from '@angular/router';
 })
 export class ClientesListaComponent implements OnInit {
   clientes: Cliente[] = [];
+  clienteSelecionado!: Cliente;
 
   constructor(
     private clienteService: ClientesService,
@@ -24,5 +25,9 @@ export class ClientesListaComponent implements OnInit {
 
   novoCadastro() {
     this.router.navigate(['/clientes-form']);
+  }
+
+  preparaDelecao(cliente: Cliente) {
+    this.clienteSelecionado = cliente;
   }
 }
